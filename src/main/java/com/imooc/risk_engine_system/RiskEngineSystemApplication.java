b06a35a2c9b471aaad3b69acf7825b05a48adc85
veritas <1820819809@qq.com>
2025-01-13 20:15:56 +0800

    commit

---
+++ src/main/java/com/imooc/risk_engine_system/RiskEngineSystemApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.risk_engine_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskEngineSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskEngineSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/risk_engine_system/RiskEngineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.risk_engine_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskEngineSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
